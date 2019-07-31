127c3c18292076ed70a77ae51bb2cebacb1a52a1
N <1000naoyuki1021@gmail.com>
2019-07-31 11:03:11 +0900

    attackメソッドをOverrideして二回攻撃を可能にした

--- rpg2/Hero.java
+++ rpg2/Hero.java
@@ -2,11 +2,19 @@ public class Hero{
 	private String name="ミナト";
 	private int hp=100;
 	public void attack(Matango m){
-		System.out.println(this.name+"の攻撃!");
+		System.out.println(this.getName()+"の攻撃!");
 		m.hp-=5;
 		System.out.println("５ポイントのダメージを与えた!");
 	}
-	public void run(){
-		System.out.println(this.name+"は逃げ出した!");
+	public final void slip(){
+		this.hp-=5;
+		System.out.println(this.getName()+"は転んだ");
+		System.out.println("５ポイントのダメージを与えた!");
+	}
+	public final void run(){
+		System.out.println(this.getName()+"は逃げ出した!");
+	}
+	public String getName(){
+		return this.name;
 	}
 }
