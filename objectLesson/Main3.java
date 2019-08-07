4e495f89254c73ad41226c3c0a790f304acf4e71
N <1000naoyuki1021@gmail.com>
2019-08-07 10:43:33 +0900

    object

---
+++ objectLesson/Main3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main3{
+	public static void main(String[] args){
+		Empty e=new Empty();
+		e.name="さぶ";
+		System.out.println(e);
+		List<Empty> list=new ArrayList<>();
+		Empty e2=new Empty();
+		e2.name="ヒロ";
+		list.add(e);
+		list.add(e2);
+		System.out.println(list);
+		if(e.equals(e2)){
+			System.out.println("同じです");
+		}else{
+			System.out.println("二つは同じではありません");
+		}
+		Empty e3=new Empty();
+		e3.name="ヒロ";
+		list.add(e);
+		list.add(e2);
+		list.add(e3);
+		if(e.equals(e3)){
+			System.out.println("同じです");
+		}else{
+			System.out.println("二つは同じではありません");
+		}
+	}
+}
